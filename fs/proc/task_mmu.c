04cd4a3240f5423d2a323a63651d257223f7013d
shenxx <shenxiangxiang@gmail.com>
2024-04-11 20:03:22 +0800

    update hide frida

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -347,6 +347,18 @@ static void show_vma_header_prefix(struct seq_file *m,
 		   MAJOR(dev), MINOR(dev), ino);
 }
 
+static int bypass_show_map_vma(struct vm_area_struct *vma) {
+        struct file *file = vma->vm_file;
+        vm_flags_t flags = vma->vm_flags;
+	if (file && file->f_path.dentry && (strstr(file->f_path.dentry->d_iname, "frida-") || strstr(file->f_path.dentry->d_iname, "/data/local/tmp/") || strstr(file->f_path.dentry->d_iname, "libhuawei.so")))
+                return 1;
+        if (file && file->f_path.dentry && strstr(file->f_path.dentry->d_iname, "libart.so") && (flags & VM_EXEC))
+		return 1;
+        if (file && file->f_path.dentry && (strstr(file->f_path.dentry->d_iname, "memfd:jit-cache") || strstr(file->f_path.dentry->d_iname, "memfd:jit-zygote-cache")))
+                return 1;
+	return 0;
+}
+
 static void
 show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 {
@@ -360,12 +372,8 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	dev_t dev = 0;
 	const char *name = NULL;
 
-	if (file && (strstr(file->f_path.dentry->d_iname, "libhuawei.so") || strstr(file->f_path.dentry->d_iname, "frida-") || strstr(file->f_path.dentry->d_iname, "data/local/tmp/")))
-		return;
-
-	if (file && strstr(file->f_path.dentry->d_iname, "libart.so") && (flags & VM_EXEC))
-		return;
-
+        if (bypass_show_map_vma(vma) == 1)
+                return;
 
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
@@ -819,6 +827,9 @@ static int show_smap(struct seq_file *m, void *v, int is_pid)
 	int ret = 0;
 	bool rollup_mode;
 	bool last_vma;
+
+	if (bypass_show_map_vma(vma) == 1)
+                return 0;
 
 	if (priv->rollup) {
 		rollup_mode = true;
